6eabe5f57514d45d5ddb0d378726fd59c191c144
vivek patel <patelvivek7351@gmail.com>
2020-08-13 19:50:13 +0530

    validation and form changes

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,7 @@
 class Student < ActiveRecord::Base
   belongs_to :standard
+  validates :name, presence: true
+  validates :roll_number, presence: true
+  validates :standard_id, presence: true
+  validates :roll_number, uniqueness: { scope: :standard_id }
 end
